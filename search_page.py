d316ac28b9d1322a0d0cc51880e7530515b37006
Puru <purupanneer@gmail.com>
2026-07-04 14:28:56 +0530

    Add Lambda handler for search gateway

--- search_page.py
+++ search_page.py
@@ -111,3 +111,11 @@ def searchhome():
 </body>
 </html>
 """
+def lambda_handler(event, context):
+    return {
+        "statusCode": 200,
+        "headers": {
+            "Content-Type": "text/html"
+        },
+        "body": searchhome()
+    }
